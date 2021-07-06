5f866bfbc6d18808cfd972c694e3d5f031fe0237
João Wicktor <joaowicktor@gmail.com>
2021-07-05 21:57:37 -0300

    Criado testes unitários de participação atual CTC Solo Cálcio/Magnésio

---
+++ src/test/java/edu/utfpr/cp/dacom/sa/soilcorrection/TestaCorrecaoCalcio.java
@@ -0,0 +1,15 @@
+package edu.utfpr.cp.dacom.sa.soilcorrection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class TestaCorrecaoCalcio {
+
+	@Test
+	void testaCalcParticipacaoAtualCTCSolo() {
+		var correcaoCalcio = new CorrecaoCalcio();
+		var atualCTCSolo = correcaoCalcio.calcParticipacaoAtualCTCSolo(0.15, 5.76, 1.63, 5.35);
+		assertEquals(44.68, atualCTCSolo, 0.01);
+	}
+}
